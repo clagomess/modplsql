8818e1a6e2a481b3485ab69d5278f93d4f761946
CLAUDIO GOMES <claudio.silva@fnde.gov.br>
2019-07-23 15:24:24 -0300

    log run

--- src/main/java/com/github/clagomess/modplsql/Main.java
+++ src/main/java/com/github/clagomess/modplsql/Main.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.clagomess.modplsql.dto.ConfigDto;
 import com.github.clagomess.modplsql.jdbc.Database;
 import com.github.clagomess.modplsql.server.ModPlsqlServer;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) throws Throwable {
         // init config
@@ -16,5 +18,7 @@ public class Main {
         // init services
         Database.init(dto);
         ModPlsqlServer.run();
+
+        log.info("Running at: http://localhost:8000");
     }
 }
